5a072c025d1dbce89d825586c378295d34703466
AKIRONMAN <34912484+AKIRONMAN@users.noreply.github.com>
2021-09-18 16:08:48 +0530

    Aamin: Student task

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB52M-vEdKBUxab8v2zYYZqNcNcjH3uFHw",
+    authDomain: "student-7b238.firebaseapp.com",
+    projectId: "student-7b238",
+    storageBucket: "student-7b238.appspot.com",
+    messagingSenderId: "740844520610",
+    appId: "1:740844520610:web:1166b2cbbd6182380a43fb"
+  }
 };
